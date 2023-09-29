5d75fecef14a9e7efadf7759b797542141249ee0
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-09-29 02:38:40 -0500

    generate: fix api url

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-talosctl gen config --with-secrets secrets.yaml --config-patch-control-plane @controlplane.common.yaml --output-types controlplane --force -o controlplane.yaml home https://api.k8s.mcswain.dev:6443
+talosctl gen config --with-secrets secrets.yaml --config-patch-control-plane @controlplane.common.yaml --output-types controlplane --force -o controlplane.yaml home https://api.k8s.jacob.network:6443
 talosctl machineconfig patch controlplane.yaml --patch @alpha.patch.yaml --output alpha.yaml
 talosctl machineconfig patch controlplane.yaml --patch @beta.patch.yaml --output beta.yaml
 talosctl machineconfig patch controlplane.yaml --patch @gamma.patch.yaml --output gamma.yaml
