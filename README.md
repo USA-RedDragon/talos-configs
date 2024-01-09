891d1d1092b120c99b5a21fd754dd698d1c14c7f
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-01-09 10:26:36 -0600

    reorganize repo

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ Created controlplane.yaml
 ```
 
 Then, apply the node configs with `talosctl -n ip.to.your.node apply-config -f nodename.yaml`
+
+## Modifying
+
+If you want to use these configs as a base for your own, you can fork this repository and modify the `controlplane.common.yaml` file to your liking and modify the extensions in `schematic.*yaml`. You can also modify the `nodename.patch.yaml` files to change the node-specific configs. If you want to add more nodes, you can create a new `nodename.patch.yaml` file and add it to the `generate.sh` script.
--- generate.sh
+++ generate.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+# Colors
 COLOR_RED='\033[0;31m'
 COLOR_GREEN='\033[0;32m'
 COLOR_YELLOW='\033[0;33m'
@@ -10,6 +11,7 @@ COLOR_CLEAR='\033[0m'
 # renovate: datasource=github-releases depName=siderolabs/talos
 TALOS_VERSION=v1.6.1
 
+# Clear any old generated files
 rm -rf alpha.yaml beta.yaml gamma.yaml controlplane.yaml
 
 # If secrets.yaml doesn't exist, create it
@@ -19,14 +21,17 @@ if [ ! -f secrets.yaml ]; then
     echo -e "${COLOR_RED}Warning: Save your secrets.yaml somewhere safe!${COLOR_CLEAR}" >&2
 fi
 
+# Use talosctl to generate the node configs
 talosctl gen config --with-secrets secrets.yaml --config-patch-control-plane @controlplane.common.yaml --output-types controlplane --force -o controlplane.yaml home https://api.k8s.jacob.network:6443
 talosctl machineconfig patch controlplane.yaml --patch @alpha.patch.yaml --output alpha.yaml
 talosctl machineconfig patch controlplane.yaml --patch @beta.patch.yaml --output beta.yaml
 talosctl machineconfig patch controlplane.yaml --patch @gamma.patch.yaml --output gamma.yaml
 
+# Use https://factory.talos.dev to generate an installer image ID
 INSTALLER_ID=$(curl -fSsL -X POST --data-binary @schematic.yaml https://factory.talos.dev/schematics | jq -r .id)
 GAMMA_INSTALLER_ID=$(curl -fSsL -X POST --data-binary @schematic.gamma.yaml https://factory.talos.dev/schematics | jq -r .id)
 
+# Use yq to set the installer image ID in the node configs
 INSTALLER_IMAGE="factory.talos.dev/installer/${INSTALLER_ID}:${TALOS_VERSION}" yq -i '.machine.install.image = strenv(INSTALLER_IMAGE)' alpha.yaml
 INSTALLER_IMAGE="factory.talos.dev/installer/${INSTALLER_ID}:${TALOS_VERSION}" yq -i '.machine.install.image = strenv(INSTALLER_IMAGE)' beta.yaml
 GAMMA_INSTALLER_IMAGE="factory.talos.dev/installer/${GAMMA_INSTALLER_ID}:${TALOS_VERSION}" yq -i '.machine.install.image = strenv(GAMMA_INSTALLER_IMAGE)' gamma.yaml
